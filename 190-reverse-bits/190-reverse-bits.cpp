52fc6f0c10d73561d0935a1bd499d059db8be4c0
Aarush Srivastava <20BCS4110@cuchd.in>
2022-01-30 12:29:18 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (51.20%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
         int ans=0;
-        for(int i=0; i<32; ++i){
+        for(int i=31; i>=0; --i){
             if((n & (1<<i))){
                 int mask = 1<<((31)-i);
                 ans = ans|mask;
