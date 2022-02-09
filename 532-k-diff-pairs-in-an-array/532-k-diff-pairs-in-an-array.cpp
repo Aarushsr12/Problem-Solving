88400121a12aa67fc1bd014ee6887807d8c80c09
Aarush Srivastava <20BCS4110@cuchd.in>
2022-02-09 13:13:42 +0530

    Time: 26 ms (51.47%), Space: 13.3 MB (58.82%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -9,7 +9,10 @@ public:
         }
         int count=0;
         for(auto it : m){
-            if((k==0 && it.second>1) || (k>0 && m.find(it.first + k) != m.end())){
+            if((k==0 && it.second>1)){
+                count++;
+            }
+            else if((k>0 && m.find(it.first + k) != m.end())){
                 count++;
             }
         }
