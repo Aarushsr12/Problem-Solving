7d1f6c457064a7dec06336e5b8947ff7ac416430
Aarush Srivastava <20BCS4110@cuchd.in>
2022-02-13 13:33:50 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (67.91%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>powerset;
+        vector<vector<int>>s;
         int n = nums.size();
         for(int i=0; i<(1<<n); ++i){
             vector<int>set;
@@ -10,8 +10,8 @@ public:
                     set.push_back(nums[bit]);
                 }
             }
-            powerset.push_back(set);
+            s.push_back(set);
         }
-        return powerset;
+        return s;
     }
 };
