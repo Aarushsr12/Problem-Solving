0242ad32a11263245b101b404838a17d0cd90e22
Aarush Srivastava <20BCS4110@cuchd.in>
2022-05-03 21:30:23 +0530

    Time: 19 ms (41.27%), Space: 18.5 MB (36.92%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -3,14 +3,13 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int n = nums.size();
         int i=0;
-        int k=1;
+        
         for(int j=1; j<nums.size(); ++j){
             if(nums[i] != nums[j]){
-                nums[k] = nums[j];     
                 i++;
-                k++;
+                nums[i] = nums[j];     
             }
         }
-        return k;
+        return i+1;
     }
 };
