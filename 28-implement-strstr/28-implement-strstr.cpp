b6645b23c59bf96a67b01f43eb208e301325b606
Aarush Srivastava <20BCS4110@cuchd.in>
2022-05-09 21:08:46 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (48.35%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -4,17 +4,14 @@ public:
         if(needle.size() == 0){
             return 0;
         }
-        char s = needle[0];
-        int m = needle.size();
         int ans = -1;
+        
         for(int i=0; i<haystack.size(); ++i){
-            if(haystack[i] == s){
                 string str = haystack.substr(i,needle.size());
                 if(str == needle){
                     ans = i; 
                     break;
                 }
-            }
         }
         return ans;
     }
