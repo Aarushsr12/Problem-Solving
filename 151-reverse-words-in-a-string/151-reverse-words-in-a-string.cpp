52d216463c72c941e6c77fbf8b0504d20a9ecd6a
Aarush Srivastava <20BCS4110@cuchd.in>
2022-05-11 12:43:04 +0530

    Time: 22 ms (15.34%), Space: 20.8 MB (17.22%) - LeetHub

---
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.size();
+        int i=0;
+        string ans="";
+        while(i < n){
+            string temp = "";
+            if(s[i] == ' '){
+                i++;
+            }
+            while(i<n && s[i] != ' '){
+                temp.push_back(s[i]);
+                i++;
+            }
+            if(temp.empty() == false){
+                ans = temp + " " + ans;
+            } 
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
