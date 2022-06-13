02b7de394b1838b27f108b8c9f0531e21e37c6c2
Aarush Srivastava <20BCS4110@cuchd.in>
2022-06-13 21:39:27 +0530

    Time: 67 ms (83.92%), Space: 21.8 MB (55.92%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int count=0;
-    bool flag = true;
     
-    void dfs(vector<vector<int>>& grid,int i,int j,int n,int m){
+    bool dfs(vector<vector<int>>& grid,int i,int j,int n,int m){
         //base case
         if(i<0 || j<0 || i>=n || j>=m){
-            flag = false;
-            return;
+            return false;
         }
         if(grid[i][j] == 0 || grid[i][j] == 2){
-            return;
+            return true;
         }
         count++;
         grid[i][j]=2;
 
-        dfs(grid,i+1,j,n,m);
-        dfs(grid,i-1,j,n,m);
-        dfs(grid,i,j-1,n,m);
-        dfs(grid,i,j+1,n,m);
+        bool d1 = dfs(grid,i+1,j,n,m);
+        bool d2 = dfs(grid,i-1,j,n,m);
+        bool d3 = dfs(grid,i,j-1,n,m);
+        bool d4 = dfs(grid,i,j+1,n,m);
         
+        return d1 && d2 && d3 && d4;
     }
     int numEnclaves(vector<vector<int>>& grid) {
         int n = grid.size();
@@ -28,10 +27,8 @@ public:
         for(int i=0; i<n; ++i){
             for(int j=0; j<m; ++j){
                 count=0;
-                flag = true;
                 if(grid[i][j] == 1){
-                    dfs(grid,i,j,n,m);
-                    if(flag){
+                    if(dfs(grid,i,j,n,m)){
                         size += count;
                     }
                 }
