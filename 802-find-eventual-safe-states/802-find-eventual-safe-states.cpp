a2c07b908af49dc32289778f21b86128eaa024b2
Aarush Srivastava <20BCS4110@cuchd.in>
2022-06-27 17:50:12 +0530

    Time: 259 ms (43.86%), Space: 46.8 MB (89.21%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
-    bool dfs(int src,vector<vector<int>> &graph,vector<int> &color){
-        //initially mark them as unsafe
-        color[src] = 1;
-        //cycle detection in directed graph
-        for(auto it : graph[src]){
-            if(color[it] == 0 && dfs(it,graph,color) || color[it] == 1){
-                return true;
+    bool dfs(int node,vector<vector<int>>& graph,vector<bool> &visited,vector<bool>                        &dfsvisited,vector<bool> &pre_cycle)
+    {
+        visited[node] = true;
+        dfsvisited[node] = true;
+        
+        for(auto it : graph[node]){
+            if(!visited[it]){
+                if(dfs(it,graph,visited,dfsvisited,pre_cycle)){
+                    return pre_cycle[node] = true;
+                }
+            }
+            else if(visited[it] && dfsvisited[it]){
+                return pre_cycle[node] = true;
             }
         }
-        
-        color[src] =2;
+        dfsvisited[node] = false;
         return false;
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int>color(n);
-        vector<int> ans;
+        vector<bool> visited(n, false);
+        vector<bool> dfsvisited(n, false);
+        vector<bool> pre_cycle(n, false);
+        
         for(int i=0; i<n; ++i){
-            if(color[i] == 2 || dfs(i,graph,color) == false){
+            if(!visited[i]){
+                dfs(i,graph,visited,dfsvisited,pre_cycle);        
+            }
+        }
+        vector<int>ans;
+        for(int i=0; i<pre_cycle.size(); ++i){
+            if(!pre_cycle[i]){
                 ans.push_back(i);
             }
         }
