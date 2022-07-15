dcebf89eb0c4826545255056ab26d1a856541c7e
Aarush Srivastava <20BCS4110@cuchd.in>
2022-07-15 13:48:16 +0530

    Time: 22 ms (49.84%), Space: 9.1 MB (21.08%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,32 +1,43 @@
 class Solution {
 public:
+    // int helper(string word1,string word2,int i,int j,vector<vector<int>> &dp){
+    //     //base case
+    //     if(i<0){
+    //         return j+1;
+    //     }
+    //     if(j<0){
+    //         return i+1;
+    //     }
+    //     if(dp[i][j] != -1){
+    //         return dp[i][j];
+    //     }
+    //     if(word1[i] == word2[j]){
+    //         return 0 + helper(word1,word2,i-1,j-1,dp);
+    //     }
+    //     return 1 + min(helper(word1,word2,i-1,j,dp),
+    //                        min(helper(word1,word2,i,j-1,dp),helper(word1,word2,i-1,j-1,dp)));
+    // }
     int minDistance(string word1, string word2) {
-        int m = word1.size();
-        int n = word2.size();
-    
-        vector<vector<int>> dp(m+1,vector<int>(n+1));
-        //1st row
-        for(int j=0; j<=n; ++j){
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int j=0; j<=m; ++j){
             dp[0][j] = j;
         }
-        //1st col
-        for(int i=1;i<=m;++i){
+        for(int i=1; i<=n; ++i){
             dp[i][0] = i;
         }
-        for(int i=1; i<=m; ++i){
-            for(int j=1; j<=n; ++j){
-                if(word1[m-i] == word2[n-j]){
+        for(int i=1; i<=n; ++i){
+            for(int j=1; j<=m; ++j){
+                //match
+                if(word1[i-1] == word2[j-1]){
                     dp[i][j] = dp[i-1][j-1];
                 }
                 else{
-                    int a = dp[i-1][j];
-                    int b = dp[i][j-1];
-                    int c = dp[i-1][j-1];
-                    dp[i][j] = 1 + min(a,min(b,c));
+                    dp[i][j] = 1 + min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1]));
                 }
             }
         }
-        return dp[m][n];
+        return dp[n][m];    
     }
-
 };
