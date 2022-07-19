dd72123224af03ffd9f41644b65a2b86439f3b8a
Aarush Srivastava <20BCS4110@cuchd.in>
2022-07-19 21:35:09 +0530

    Time: 7 ms (84.01%), Space: 13 MB (88.78%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -3,8 +3,9 @@ public:
     int maxProfit(vector<int>& prices) {
         int profit = 0;
         for(int i=1; i<prices.size(); ++i){
-            profit += max(prices[i]-prices[i], prices[i] - prices[i-1]);
-            cout<<profit<<" ";
+            if(prices[i] > prices[i-1]){
+                profit += max(0, prices[i] - prices[i-1]);
+            } 
         }
         return profit;
     }
