2566db06c05c84d9987a5c703ab27fa157511a73
Aarush Srivastava <20BCS4110@cuchd.in>
2023-05-04 11:20:31 +0530

    Time: 8 ms (72.14%), Space: 8.2 MB (12.21%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -1,43 +1,34 @@
 class Solution {
 public:
-    void ban(string &senate,vector<bool> &banned,char ban,int start){
-        while(true){
-            if(senate[start]==ban && !banned[start]){
-                banned[start] = true;
-                break;
-            }
-            start = (start + 1 ) % senate.size();
-        }
-    }
     string predictPartyVictory(string senate) {
         int n = senate.size();
-        vector<bool> banned(n, false);
-        int rCount = 0;
-        int dCount = 0;
-        //count of senates
-        for(int i=0;  i<n; ++i){
+        queue<int> dq;
+        queue<int> rq;
+        
+        for(int i=0; i<n; ++i){
             if(senate[i] == 'R'){
-                rCount++;
+                rq.push(i);
             }
             else{
-                dCount++;
+                dq.push(i);
             }
         }
-        //now at last only one senate members should remain
-        int i = 0;
-        while(rCount > 0 && dCount > 0){
-            if(banned[i] == false){
-                if(senate[i] == 'R'){
-                    ban(senate,banned,'D',(i+1) % n);
-                    dCount--;
-                }
-                else{
-                    ban(senate,banned,'R',(i+1) % n);
-                    rCount--;
-                }
+        while(!rq.empty() && !dq.empty()){
+            int index1 = rq.front();
+            int index2 = dq.front();
+            
+            //remove both the elements
+            rq.pop();
+            dq.pop();
+            
+            if(index1 < index2){
+                rq.push(index1 + n);
+            }            
+            else{
+                dq.push(index2 + n);
             }
-            i = (i+1) % n;
         }
-        return (rCount == 0) ? "Dire" : "Radiant";
+        
+        return (rq.empty() ==  true) ? "Dire" : "Radiant";
     }
 };
