5fbbccb027a6970cf7ef859d3a159329e8df900d
Aarush Srivastava <20BCS4110@cuchd.in>
2023-05-13 01:30:52 +0530

    Time: 91 ms (23.72%), Space: 24.6 MB (13.00%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -7,7 +7,7 @@ public:
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         for(int i=1; i<=n; ++i){
             for(int j=1; j<=m; ++j){
-                if(text1[i-1] == text2[j-1]){
+                if(text1[n-i] == text2[m-j]){
                     dp[i][j] = 1 + dp[i-1][j-1];
                 }
                 else{
